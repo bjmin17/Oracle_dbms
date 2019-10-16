0876a333a3f7dbc01acc19a6e53297e0b2e6510e
bjmin17 <bjmin17@naver.com>
2019-10-16 15:57:45 +0900

    제1정규화, 제2정규화 연습,, 테이블 칼럼 추가 및 이름 변경

--- GRADE(2019-10-15-정규화).sql
+++ GRADE(2019-10-15-정규화).sql
@@ -151,3 +151,39 @@ SELECT * FROM tbl_score;
 SELECT s_std, s_subject, SB.sb_name, SB.sb_pro, s_score
 FROM tbl_score SC, tbl_subject SB
     WHERE SC.s_subject = SB.sb_code;
+    
+    
+-- TABLE을 JOIN 할 때 TABLE들에 칼럼 이름이 같은 이름이 존재하면
+-- 반드시 칼럼이 어떤 TABLE에 있는 칼럼인지 명시를 해주어야
+-- 문법적 오류가 발생하지 않는다.
+-- 그래서 TABLE을 설계할 때 가급적 접두사를 붙여서 만드는 것이 좋고
+-- 그렇더라도 JOIN을 할 때 TABLE alias를 설정하여
+-- alias.Column 형식으로 작성하는 것이 좋다.
+/*
+Table1 : num, name, addr, dept
+Table2 : num, subject, pro
+
+SELECT *
+FROM Table1, Table2
+    WHERE dept = num
+라는 형식으로 SQL을 작성하면 num 칼럼이 누구의 Table인지 알 수 없어서
+문법적 오류가 발생한다
+
+SELECT T1.num AS 학번, T1.name, T1.addr, T1.dept,
+        T2.num AS 과목코드, T2.subject, T2. pro
+FROM Table1 T1, Table2 T2
+    WHERE T1.dept = T2.num
+    
+와 같은 형식으로 SQL을 작성하는 것이 만약의 오류를 줄일 수 있다.
+
+*/
+SELECT s_id, s_std, s_subject, s_score, s_rem FROM tbl_score;
+
+SELECT *
+FROM tbl_score
+WHERE s_id > 600;
+
+DELETE
+FROM tbl_score
+WHERE s_id > 600;
+COMMIT;
